b3997167d0dca7c653383ff35efee8772c9b3369
Lee Kol <79585933+leeKol@users.noreply.github.com>
2022-01-06 10:57:33 +0200

    Drawer Navbar
    Created:
    Settings Fragment- frame to be navigated.
    nav_drawer_menu + nav_header- implement the drawer navbar.
    
    Modified:
    Home activity, functionality, to navigate between fragments, has been added.
    activity_home- the drawer nav bar was added to the design.
    
    Deleted:
    nav_item_color_selected- it's functionality is not required anymore.

--- app/src/main/java/com/example/babysitbook/HomeActivity.kt
+++ app/src/main/java/com/example/babysitbook/HomeActivity.kt
@@ -3,7 +3,11 @@ package com.example.babysitbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -14,6 +18,7 @@ import com.example.babysitbook.fragments.HomeFragment
 import com.example.babysitbook.fragments.ChatFragment
 import com.example.babysitbook.fragments.ProfileFragment
 import com.example.babysitbook.fragments.CalendarFragment
+import com.example.babysitbook.fragments.SettingsFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -21,6 +26,7 @@ class HomeActivity : AppCompatActivity() {
     //private val database = Firebase.database("https://babysitbook-4e036-default-rtdb.europe-west1.firebasedatabase.app")
     //private val myRef = database.getReference("Users/Babysiter")
     private lateinit var auth: FirebaseAuth
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,41 +38,70 @@ class HomeActivity : AppCompatActivity() {
         val favoriteFragment = FavoriteFragment()
         val calendarFragment = CalendarFragment()
         val profileFragment = ProfileFragment()
+        val settingsFragment = SettingsFragment()
 
-        setCurrentFragment(homeFragment)
+        setCurrentFragment(homeFragment, "Home")
 
         bottom_navigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.nav_home -> {
-                    setCurrentFragment(homeFragment)
+                    setCurrentFragment(homeFragment, "Home")
                 }
                 R.id.nav_chat -> {
-                    setCurrentFragment(chatFragment)
+                    setCurrentFragment(chatFragment, "Chat")
                 }
                 R.id.nav_favorite -> {
-                    setCurrentFragment(favoriteFragment)
+                    setCurrentFragment(favoriteFragment, "Favorites")
                 }
                 R.id.nav_calendar -> {
-                    setCurrentFragment(calendarFragment)
+                    setCurrentFragment(calendarFragment, "Calendar")
                 }
                 R.id.nav_profile -> {
-                    setCurrentFragment(profileFragment)
+                    setCurrentFragment(profileFragment, "Profile")
                 }
             }
             true
         }
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.OPEN, R.string.CLOSE)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nav_drawer_view.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            when(it.itemId) {
+                R.id.mi_settings -> {
+                    setCurrentFragment(settingsFragment, "Settings")
+                }
+
+                R.id.mi_logout -> Toast.makeText(applicationContext,
+                    "Clicked Logout", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 
-    private fun setCurrentFragment(fragment: Fragment) =
+    private fun setCurrentFragment(fragment: Fragment, title: String) =
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fl_wrapper, fragment)
             commit()
+
+            supportActionBar?.title= title
         }
 
 
     fun logout(view: View){
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
