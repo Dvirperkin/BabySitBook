3f6338cc8163cab58b39a2ab2aad638c4f53dc88
Almog <almoglevi1008@gmail.com>
2022-03-23 21:16:40 +0200

    Created: xml files for calendar implementations.

--- app/src/main/java/com/example/babysitbook/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/babysitbook/fragments/CalendarFragment.kt
@@ -1,20 +1,8 @@
 package com.example.babysitbook.fragments
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.babysitbook.databinding.FragmentCalendarBinding
+
 
 class CalendarFragment : Fragment(){
-    lateinit var binding: FragmentCalendarBinding
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding= FragmentCalendarBinding.inflate(inflater)
-        return binding.root
-    }
+
 }
